b5fe4fecaffc771cae45228a5ef278ee425581fe
Dennis Lee <wylee2000@gmail.com>
2018-12-19 18:24:10 +0800

    fixed prevent shutdown/reboot bug support external IP in message body when internet resumed and application starts, closed #1 trim leading zero in datetime for more human friendly remove newold library (no long needed) added ExternalIp, State, DownSince, UpSince & Duration feature in internet library improved internet down datetime detection

--- main.py
+++ main.py
@@ -7,13 +7,12 @@ from ago import human
 from datetime import datetime
 from ConfigParser import SafeConfigParser
 from lib.internet import Internet
-from lib.newold import NewOld
 
 def main():
     try:
-        available = NewOld(onChangeCb=onNetStatusChanged)
         while True:
-            available.Value = Internet().Reachable(SITES)
+            internet.Reachable(SITES)
+            #log.debug("Internet %s for %s" % (("up" if internet.State else "down"), human(internet.Duration, 2, past_tense='{0}')))
             time.sleep(REFRESH)
 
     except KeyboardInterrupt:
@@ -22,24 +21,29 @@ def main():
     except Exception as e:
         log.error(e, exc_info=True)
 
-def onNetStatusChanged(available):
-    global dt_down
-
-    if available.New:
-        dt_up = datetime.now()
+#region Events
+def onStateChanged(sender, available):
+    if available:
         log.debug("Internet is up")
 
-        diff = dt_up - dt_down
+        diff = sender.UpSince - sender.DownSince
 
         # if downtime was less than a day, no need include date in the message body
         if diff.days < 1:
             precision = 2 if diff.total_seconds() > 60 else 1
             title = "Internet resumed, it was down for %s" % human(diff, precision, past_tense='{0}')
-            body = "Internet resumed on %s, it was down since %s for %s" % (dt_up.strftime("%I:%M:%S%p"), dt_down.strftime("%I:%M:%S%p"), human(diff, precision, past_tense='{0}'))
+            body = "Internet resumed back at %s, it was down since %s for %s" % \
+                   (_trimDateTimeLeadingZero(sender.UpSince.strftime("%I:%M:%S%p")),
+                    _trimDateTimeLeadingZero(sender.DownSince.strftime("%I:%M:%S%p")), human(diff, precision, past_tense='{0}'))
         else:
             title = "Internet resumed, it was down for %s" % human(diff, 3, past_tense='{0}')
-            body = "Internet resumed on %s, it was down since %s for %s" % (dt_up.strftime("%d/%m/%y %I:%M:%S%p"), dt_down.strftime("%d/%m/%y %I:%M:%S%p"), human(diff, 3, past_tense='{0}'))
+            body = "Internet resumed on %s, it was down since %s for %s" % \
+                   (_trimDateTimeLeadingZero(sender.UpSince.strftime("%d/%m/%y %I:%M:%S%p")),
+                    _trimDateTimeLeadingZero(sender.DownSince.strftime("%d/%m/%y %I:%M:%S%p")), human(diff, 3, past_tense='{0}'))
+
         log.info(title)
+
+        if EXTERNALIP: body += ". External IP: %s" % internet.ExternalIp()
 
         # notify all of the services loaded into our Apprise object
         apobj.notify(
@@ -47,18 +51,28 @@ def onNetStatusChanged(available):
             body=body,
         )
     else:
-        dt_down = datetime.now()
-        log.info("Internet is down")
+        log.info("Internet is down since %s" % sender.DownSince.strftime("%I:%M:%S%p"))
 
 def onTerminate(signum, frame):
     log.info("Application terminated (OS shutdown/reboot)")
+    exit(1)
 
-def addNotificationService(cfgParser):
+#endregion
+
+#region Private methods
+
+def _addNotificationService(cfgParser):
     """Add all of the notification services by their server url"""
     for option in cfgParser.options('apprise'):
         value = cfgParser.get('apprise', option)
-        if not value:
+        if value:
+            log.debug("Adding services '%s://%s'" % (option, value))
             apobj.add("%s://%s" % (option, value))
+
+def _trimDateTimeLeadingZero(dt):
+    return dt.lstrip("0").replace(" 0", " ")
+
+#endregion
 
 if __name__ == "__main__":
     currentPath = os.path.dirname(os.path.abspath(__file__))
@@ -69,6 +83,7 @@ if __name__ == "__main__":
     DEBUG = cfgParser.getboolean('debugging', 'debug')
     SITES = [x.strip() for x in cfgParser.get('availability', 'sites').split(',')]
     REFRESH = cfgParser.getint('availability', 'refresh')
+    EXTERNALIP = cfgParser.getboolean('general', 'include_external_ip')
 
     # region logging
     # initialize logging
@@ -86,6 +101,8 @@ if __name__ == "__main__":
     log.info("Starting Internet Uptime Monitor%s" % (' (DEBUG mode)' if DEBUG else ''))
     # endregion
 
+    internet = Internet(onStateChanged)
+
     # Python detect linux shutdown and run a command before shutting down
     # credits to code_onkel
     # https://stackoverflow.com/questions/39275948/python-detect-linux-shutdown-and-run-a-command-before-shutting-down
@@ -95,6 +112,14 @@ if __name__ == "__main__":
     apobj = apprise.Apprise()
 
     # add Apprise notification settings
-    addNotificationService(cfgParser)
+    _addNotificationService(cfgParser)
+
+    # application started
+    body = "Application started since %s" % _trimDateTimeLeadingZero(datetime.now().strftime("%d/%m/%y %I:%M:%S%p"))
+    if EXTERNALIP: body += ". External IP: %s" % internet.ExternalIp()
+    apobj.notify(
+        title="Internet Uptime Monitor started",
+        body=body,
+    )
 
     main()
